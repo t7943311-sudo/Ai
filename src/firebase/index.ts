c0d49e7933576c8c381d9df278454688c9c53a33
Tech <t7943311@gmail.com>
2026-02-08 04:28:09 +0000

    Try fixing this error: `Runtime FirebaseError: Firebase: Error (auth/inv

--- src/firebase/index.ts
+++ src/firebase/index.ts
@@ -6,14 +6,20 @@ import { FirebaseProvider, useFirebase } from './provider';
 import { FirebaseClientProvider } from './client-provider';
 import { useUser } from './auth/use-user';
 
-
 function initializeFirebase(): {
   app: FirebaseApp;
   auth: Auth;
   firestore: Firestore;
 } {
-  // Defer importing config until we are on the client
-  const { firebaseConfig } = require('./config');
+  // These variables are only accessed on the client, so they are safe.
+  const firebaseConfig = {
+    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  };
 
   if (getApps().length) {
     const app = getApp();
